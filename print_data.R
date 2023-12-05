76d18cf350998fda7f440edc7eeaf4cd02dd3fb9
hauschke <christian.hauschke@tib.eu>
2023-12-05 15:12:53 +0100

    Create print_data.R

---
+++ print_data.R
@@ -0,0 +1,3 @@
+data=read.csv("https://raw.githubusercontent.com/Sabishka10101/UA_Academic_Events/main/What%20are%20the%20most%20important%20metadata%20fields%20for%20academic%20events%20(5%20most%20important%20in%20your%20opinion%20(multiple%20choice%20answers).csv")
+
+print(data)
